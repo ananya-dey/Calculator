92472c2e4519444b7ae7d459eb7ee2efb33e1e60
anna67 <ananyadey067@gmail.com>
2016-01-07 23:11:39 -0800

    It is a simple calculator
    Using java i developed this calculator where user can sum multiply
    divide subtract any number,

---
+++ src/calculation/Calculation.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculation;
+
+/**
+ *
+ * @author Ayon
+ */
+public class Calculation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        calc c = new calc();
+        c.setLocation(300, 200);
+        c.setVisible(true);
+    }
+}
